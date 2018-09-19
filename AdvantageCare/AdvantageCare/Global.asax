d25e03d8973f48f368efc0fec4cd7ebb142677a1
sumayyanavas <sumayyanavas@gmail.com>
2018-09-19 09:37:10 -0400

    firstcommit

---
+++ AdvantageCare/AdvantageCare/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.vb" Inherits="AdvantageCare.Global_asax" Language="vb" %>
---
+++ AdvantageCare/AdvantageCare/LoginPage.aspx.vb
@@ -0,0 +1,12 @@
+﻿Public Class LoginPage
+    Inherits System.Web.UI.Page
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+
+    End Sub
+    Protected Sub btnLogin_Click(sender As Object, e As EventArgs) Handles btnLogin.Click
+
+        Response.Redirect("ContactList.aspx")
+
+    End Sub
+End Class
